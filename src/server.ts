20b8f17dec2f9061a6fd2df9e32e27bd2dfcf83f
chalermsak-s <kaidohjung@gmail.com>
2025-02-17 00:17:36 +0700

    Lab 3 Task 2.1- 2.4

--- src/server.ts
+++ src/server.ts
@@ -1,89 +1,10 @@
-import { group } from 'console'
 import express, { Request, Response } from 'express'
+import { getAllEvents, getEventByCategory, getEventById, addEvent } from "./services/eventService";
+import type { Event} from "./services/eventService";
+
 const app = express()
 app.use(express.json())
 const port = 3000
-
-interface Event {
-  id: number
-  category: string
-  title: string
-  description: string
-  location: string
-  date: string
-  time: string
-  petsAllowed: boolean
-  organizer: string
-}
-
-const events: Event[] = [
-  {
-    id: 1,
-    category: 'Music',
-    title: 'Concert',
-    description: 'A live concert',
-    location: 'London',
-    date: '2021-07-01',
-    time: '19:00',
-    petsAllowed: false,
-    organizer: 'Live Nation',
-  },
-  {
-    id: 2,
-    category: 'Art',
-    title: 'Art Exhibition',
-    description: 'An exhibition of modern art',
-    location: 'Paris',
-    date: '2021-08-15',
-    time: '10:00',
-    petsAllowed: true,
-    organizer: 'Art World',
-  },
-  {
-    id: 3,
-    category: 'Technology',
-    title: 'Tech Conference',
-    description: 'A conference on the latest in tech',
-    location: 'San Francisco',
-    date: '2021-09-10',
-    time: '09:00',
-    petsAllowed: false,
-    organizer: 'Tech Innovators',
-  },
-  {
-    id: 4,
-    category: 'Sports',
-    title: 'Marathon',
-    description: 'A city-wide marathon',
-    location: 'New York',
-    date: '2021-10-05',
-    time: '07:00',
-    petsAllowed: true,
-    organizer: 'RunNYC',
-  },
-  {
-    id: 5,
-    category: 'Food',
-    title: 'Food Festival',
-    description: 'A festival of food and drinks',
-    location: 'Tokyo',
-    date: '2021-11-20',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Foodies United',
-  },
-  {
-    id: 6,
-    category: 'Film',
-    title: 'Film Premiere',
-    description: 'Premiere of a new blockbuster',
-    location: 'Los Angeles',
-    date: '2021-12-01',
-    time: '18:00',
-    petsAllowed: false,
-    organizer: 'Hollywood Studios',
-  },
-]
 
 interface Book {
   id: number
@@ -136,25 +57,6 @@ const books: Book[] = [
     groups: ['Fiction', 'Romance'],
   },
 ]
-
-function getEventByCategory(category: string): Event[] {
-  const filteredEvents = events.filter((event) => event.category === category)
-  return filteredEvents
-}
-
-function getAllEvents(): Event[] {
-  return events
-}
-
-function getEventById(id: number): Event | undefined {
-  return events.find((event) => event.id === id)
-}
-
-function addEvent(newEvent: Event): Event {
-  newEvent.id = events.length + 1
-  events.push(newEvent)
-  return newEvent
-}
 
 function getBookByGroups(groups: string): Book[] {
   const filteredBooks = books.filter((book) => book.groups.includes(groups))

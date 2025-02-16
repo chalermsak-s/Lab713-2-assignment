7acbaf83cc2114dc71552f0732d5b0357e834df0
chalermsak-s <kaidohjung@gmail.com>
2025-02-17 01:53:12 +0700

    Lab 3 Task 11.1 - 11.2

--- src/services/uploadFileService.ts
+++ src/services/uploadFileService.ts
@@ -1,25 +1,27 @@
-import s3Client from '../awsConfig'
-import { PutObjectCommand } from '@aws-sdk/client-s3'
+import s3Client from '../awsConfig';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { randomBytes } from 'crypto';
 
-export async function uploadFile(
-  bucket: string,
-  filePath: string,
-  file: Express.Multer.File
-): Promise<string> {
+function generateSaltedFilename(originalName: string): string {
+  const salt = randomBytes(16).toString('hex');
+  const extension = originalName.split('.').pop();
+  return `${salt}.${extension}`;
+}
+
+export async function uploadFile(bucket: string, filePath: string, file: Express.Multer.File): Promise<string> {
+  const saltedFilename = generateSaltedFilename(file.originalname);
+  const saltedFilePath = `${filePath}/${saltedFilename}`;
   const params = {
     Bucket: bucket,
-    Key: filePath,
+    Key: saltedFilePath,
     Body: file.buffer,
     ContentType: file.mimetype,
-  }
-  try {
-    const data = await s3Client.send(new PutObjectCommand(params))
-    console.log('File uploaded successfully:', data)
-    const publicUrl = `https://qiwwqmigxpucshdwfzup.supabase.co/storage/v1/object/public/images/${filePath}`
-    console.log('File uploaded successfully:', publicUrl)
-    return publicUrl
-  } catch (error) {
-    console.error('Error uploading file:', error)
-    throw error
-  }
+  };
+
+  const data = await s3Client.send(new PutObjectCommand(params));
+  console.log('File uploaded successfully:', data);
+  const publicUrl = `https://qiwwqmigxpucshdwfzup.supabase.co/storage/v1/object/public/images/${saltedFilePath}`;
+  console.log('File uploaded successfully:', publicUrl);
+  return publicUrl;
 }
+

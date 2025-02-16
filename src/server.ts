234174abf924a883e153207627da226d1b39113a
chalermsak-s <kaidohjung@gmail.com>
2025-02-17 00:20:47 +0700

    Lab 3 Task 2.5

--- src/server.ts
+++ src/server.ts
@@ -1,81 +1,12 @@
 import express, { Request, Response } from 'express'
 import { getAllEvents, getEventByCategory, getEventById, addEvent } from "./services/eventService";
 import type { Event} from "./services/eventService";
+import { getAllBooks, getBookByGroups, getBookById, addBook } from "./services/bookService";
+import type { Book } from "./services/bookService";
 
 const app = express()
 app.use(express.json())
 const port = 3000
-
-interface Book {
-  id: number
-  title: string
-  author_name: string
-  description: string
-  groups: string[]
-}
-const books: Book[] = [
-  {
-    id: 1,
-    title: 'The Great Gatsby',
-    author_name: 'F. Scott Fitzgerald',
-    description: 'A novel set in the Jazz Age',
-    groups: ['Fiction', 'Classic'],
-  },
-  {
-    id: 2,
-    title: 'To Kill a Mockingbird',
-    author_name: 'Harper Lee',
-    description: 'A novel about racial injustice',
-    groups: ['Fiction', 'Classic'],
-  },
-  {
-    id: 3,
-    title: '1984',
-    author_name: 'George Orwell',
-    description: 'A dystopian novel',
-    groups: ['Fiction', 'Dystopian'],
-  },
-  {
-    id: 4,
-    title: 'The Catcher in the Rye',
-    author_name: 'J.D. Salinger',
-    description: 'A novel about teenage rebellion',
-    groups: ['Fiction', 'Classic'],
-  },
-  {
-    id: 5,
-    title: 'Moby-Dick',
-    author_name: 'Herman Melville',
-    description: "A novel about a sea captain's obsession",
-    groups: ['Fiction', 'Adventure'],
-  },
-  {
-    id: 6,
-    title: 'Pride and Prejudice',
-    author_name: 'Jane Austen',
-    description: 'A novel about manners and marriage',
-    groups: ['Fiction', 'Romance'],
-  },
-]
-
-function getBookByGroups(groups: string): Book[] {
-  const filteredBooks = books.filter((book) => book.groups.includes(groups))
-  return filteredBooks
-}
-
-function getAllBooks(): Book[] {
-  return books
-}
-
-function getBookById(id: number): Book | undefined {
-  return books.find((book) => book.id === id)
-}
-
-function addBook(newBook: Book): Book {
-  newBook.id = books.length + 1
-  books.push(newBook)
-  return newBook
-}
 
 app.get('/events', (req: Request, res: Response) => {
   if (req.query.category) {

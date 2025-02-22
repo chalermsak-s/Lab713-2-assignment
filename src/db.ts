a96cac62794e0e68b7253cee16aceeef3a7cfe01
chalermsak-s <kaidohjung@gmail.com>
2025-02-22 10:51:01 +0700

    Lab 04 ORM Task 1.1-1.5

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,11 @@
 import mysql from 'mysql2/promise'
+import dotenv from 'dotenv'
+dotenv.config()
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'rootpassword',
-  database: 'events',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
